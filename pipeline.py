82a32782a41d3b571ee211adcd997c3f05972942
David Cavanaugh <dcavanau@us.ibm.com>
2021-08-03 08:51:17 -0400

    point to my repository

--- pipeline.py
+++ pipeline.py
@@ -59,8 +59,8 @@ def train_and_eval_op(image: str, pvolume: PipelineVolume, data_dir: str, ):
     name='Fashion MNIST Training Pipeline',
     description='Fashion MNIST Training Pipeline to be executed on KubeFlow.'
 )
-def training_pipeline(image: str = 'benjamintanweihao/kubeflow-mnist',
-                      repo_url: str = 'https://github.com/benjamintanweihao/kubeflow-mnist.git',
+def training_pipeline(image: str = 'dcavanau/kubeflow-mnist',
+                      repo_url: str = 'https://github.ibm.com/dcavanau/kubeflow-mnist.git',
                       data_dir: str = '/workspace'):
     git_clone = git_clone_op(repo_url=repo_url)
 
